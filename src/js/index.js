d6057cd08143ce2847d2e39826defc39e11a0020
Kaio Alves <118333661+Kbalves@users.noreply.github.com>
2023-09-08 00:02:16 +0000

    Simple Counter finished by Kaio

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,10 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
 
-// include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
 let counter = 0;
 const data = () => {
   const four = Math.floor(counter / 10000);
